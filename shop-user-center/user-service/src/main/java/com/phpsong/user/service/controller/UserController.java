b311991e6252798784b280568424389cce35ad31
qiuyisong <454213722@qq.com>
2019-11-19 18:06:51 +0800

    用户中心

--- shop-user-center/user-service/src/main/java/com/phpsong/user/service/controller/UserController.java
+++ shop-user-center/user-service/src/main/java/com/phpsong/user/service/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.phpsong.user.service.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * @Author: qiuyisong
@@ -9,5 +13,51 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
 
+    /**
+     * 校验数据
+     * @param data
+     * @param type
+     * @return
+     */
+    @GetMapping("/check/{data}/{type}")
+    public ResponseEntity<Boolean> checkData(@PathVariable("data") String data, @PathVariable("type") Integer type) {
+        return ResponseEntity.ok(userService.checkData(data, type));
+    }
+
+    /**
+     * 发送验证码
+     * @param phone
+     * @return
+     */
+    @PostMapping("send")
+    public ResponseEntity<Void> sendVerifyCode(@RequestParam("phone")String phone) {
+        userService.sendVerifyCode(phone);
+        return ResponseEntity.noContent().build();
+    }
+
+    /**
+     * 用户注册
+     * @param user
+     * @param code
+     * @return
+     */
+    @PostMapping("register")
+    public ResponseEntity<Void> register(@Valid User user, @RequestParam("code")String code) {
+        userService.register(user, code);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    /**
+     * 根据用户名和密码查询用户
+     * @param username
+     * @param password
+     * @return
+     */
+    @GetMapping("query")
+    public ResponseEntity<User> queryUser(@RequestParam("username") String username, @RequestParam("password") String password) {
+        return ResponseEntity.ok(userService.queryUser(username, password));
+    }
 }
