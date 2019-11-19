f9fb68e4bf153dbaf0bad9fc4ce9db35c7993ebb
qiuyisong <454213722@qq.com>
2019-11-19 16:49:08 +0800

    添加mysql

---
+++ shop-user-center/user-service/src/main/java/com/phpsong/user/service/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.phpsong.user.service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: qiuyisong
+ * @Date: 2019/11/19 16:12
+ * 用户控制器
+ */
+@RestController
+public class UserController {
+
+}
